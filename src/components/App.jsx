8d4739bf0357c4cabc124eacf3529dc09267c705
Roman <77742141+rumpear@users.noreply.github.com>
2022-04-30 14:42:14 +0300

    upd imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React template
-    </div>
-  );
-};
+import { Section } from './Section';
+
+import { Profile } from './Profile';
+import user from '../data/user.json';
+
+import { Statistics } from './Statistics';
+import data from '../data/data.json';
+
+import { FriendList } from './FriendList';
+import friends from '../data/friends.json';
+
+import { TransactionHistory } from './TransactionHistory';
+import transactions from '../data/transactions.json';
+
+const App = () => (
+  <Section>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
+  </Section>
+);
+
+export default App;
